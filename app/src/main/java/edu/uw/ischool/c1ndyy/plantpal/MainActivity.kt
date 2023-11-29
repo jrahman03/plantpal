4bca3bad0cc8f4d8344b7227ab32f313f6eff175
c1ndyyang <c1ndyy@uw.edu>
2023-11-29 15:49:09 -0800

    add kotlin stuff

---
+++ app/src/main/java/edu/uw/ischool/c1ndyy/plantpal/MainActivity.kt
@@ -0,0 +1,11 @@
+package edu.uw.ischool.c1ndyy.plantpal
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
