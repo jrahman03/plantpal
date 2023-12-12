5480ea60ea16be15f0a9e6413e31252b8f1cb6a5
Yue Cao <yc324@uw.edu>
2023-12-11 20:56:45 -0800

    can add to json file

--- app/src/main/java/edu/uw/ischool/c1ndyy/plantpal/AddPlant.kt
+++ app/src/main/java/edu/uw/ischool/c1ndyy/plantpal/AddPlant.kt
@@ -1,15 +1,54 @@
 package edu.uw.ischool.c1ndyy.plantpal
+
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import org.json.JSONArray
+import org.json.JSONObject
+
 class AddPlant : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_add_plant) // Replace with your actual layout resource ID
+        setContentView(R.layout.activity_add_plant)
 
+        val editTextPlantName = findViewById<EditText>(R.id.editTextPlantName)
+        val editTextDescription = findViewById<EditText>(R.id.editTextDescription)
+        val editTextType = findViewById<EditText>(R.id.editTextType)
+        val editTextCareMethods = findViewById<EditText>(R.id.editTextCareMethods)
         val buttonAddPlant = findViewById<Button>(R.id.buttonAddPlant)
+
         buttonAddPlant.setOnClickListener {
-            // Handle the click event for adding a plant
+            val plantName = editTextPlantName.text.toString()
+            val description = editTextDescription.text.toString()
+            val type = editTextType.text.toString()
+            val careMethods = editTextCareMethods.text.toString()
+
+            if (plantName.isNotEmpty() && description.isNotEmpty() && type.isNotEmpty() && careMethods.isNotEmpty()) {
+                val newPlant = JSONObject().apply {
+                    put("name", plantName)
+                    put("description", description)
+                    put("type", type)
+                    put("care_methods", careMethods)
+                }
+
+                // Now add this to the existing list and save it in SharedPreferences
+                val sharedPreferences = getSharedPreferences("PlantPalData", MODE_PRIVATE)
+                val plantsJsonString = sharedPreferences.getString("Plants", "[]")
+                val plantsJsonArray = JSONArray(plantsJsonString)
+                plantsJsonArray.put(newPlant)
+
+                with(sharedPreferences.edit()) {
+                    putString("Plants", plantsJsonArray.toString())
+                    apply()
+                }
+
+                Toast.makeText(this, "Plant added successfully", Toast.LENGTH_SHORT).show()
+                finish() // Close this activity and go back to the previous one
+            } else {
+                Toast.makeText(this, "Please fill in all fields", Toast.LENGTH_SHORT).show()
+            }
         }
     }
 }
