eebe0039de1eee123e49d321c3a0305b0a8e5b09
Yue Cao <yc324@uw.edu>
2023-12-15 07:34:45 +0800

    add explore

--- app/src/main/java/edu/uw/ischool/c1ndyy/plantpal/ExplorePlant.kt
+++ app/src/main/java/edu/uw/ischool/c1ndyy/plantpal/ExplorePlant.kt
@@ -3,66 +3,71 @@ package edu.uw.ischool.c1ndyy.plantpal
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
-import android.content.Intent
-import android.util.Log
-import android.widget.Button
+import android.widget.ArrayAdapter
+import java.io.IOException
+import org.json.JSONArray
+import android.widget.ListView
+import android.widget.LinearLayout
+
+
 
 class ExplorePlant : AppCompatActivity() {
+    private lateinit var plantsListView: ListView
+    private lateinit var alphabetIndexLayout: LinearLayout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_exploreplant)
 
-        val exploreCommonNames = intent.getStringArrayListExtra("commonNames")
+        plantsListView = findViewById(R.id.plantsListView)
+        alphabetIndexLayout = findViewById(R.id.alphabetIndexLayout)
 
-        // Use the data
-        val textView1 = findViewById<TextView>(R.id.explorePlant1)
-        val textView2 = findViewById<TextView>(R.id.explorePlant2)
-        val textView3 = findViewById<TextView>(R.id.explorePlant3)
-        val textView4 = findViewById<TextView>(R.id.explorePlant4)
-        val textView5 = findViewById<TextView>(R.id.explorePlant5)
-        val textView6 = findViewById<TextView>(R.id.explorePlant6)
-        val textView7 = findViewById<TextView>(R.id.explorePlant7)
-        val textView8 = findViewById<TextView>(R.id.explorePlant8)
-        val textView9 = findViewById<TextView>(R.id.explorePlant9)
-        val textView10 = findViewById<TextView>(R.id.explorePlant10)
-        textView1.text = "(Load data (e.g. 'plants.json') in settings to explore plants)"
+        val latinNames = loadJsonData()?.let { parseJsonAndExtractLatinNames(it) }?.sorted() ?: listOf()
 
-        if (exploreCommonNames != null) {
-            // val testingName = exploreCommonNames[1].toString()
-            // Log.d("name", testingName.toString())
+        val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, latinNames)
+        plantsListView.adapter = adapter
 
-            textView1.text = exploreCommonNames[0].toString()
-            textView2.text = exploreCommonNames[1].toString()
-            textView3.text = exploreCommonNames[2].toString()
-            textView4.text = exploreCommonNames[3].toString()
-            textView5.text = exploreCommonNames[4].toString()
-            textView6.text = exploreCommonNames[5].toString()
-            textView7.text = exploreCommonNames[6].toString()
-            textView8.text = exploreCommonNames[7].toString()
-            textView9.text = exploreCommonNames[8].toString()
-            textView10.text = exploreCommonNames[9].toString()
-        } else {
-            textView1.text = "(Load data (e.g. 'plants.json') in settings to explore plants)"
+
+        setupAlphabetIndex(latinNames)
+    }
+
+    private fun loadJsonData(): String? {
+        return try {
+            assets.open("plants.json").bufferedReader().use { it.readText() }
+        } catch (ioException: IOException) {
+            ioException.printStackTrace()
+            null
         }
+    }
 
-
-
-        val navSettings = findViewById<Button>(R.id.settings)
-        navSettings.setOnClickListener {
-            val intent = Intent(this, SettingsActivity::class.java)
-            startActivity(intent)
+    private fun parseJsonAndExtractLatinNames(jsonString: String): List<String> {
+        val latinNames = mutableListOf<String>()
+        val jsonArray = JSONArray(jsonString)
+        for (i in 0 until jsonArray.length()) {
+            val jsonObject = jsonArray.getJSONObject(i)
+            val latin = jsonObject.getString("latin")
+            latinNames.add(latin)
         }
+        return latinNames
+    }
 
-        val navHome = findViewById<Button>(R.id.home)
-        navHome.setOnClickListener {
-            val intent = Intent(this, MainActivity::class.java)
-            startActivity(intent)
-        }
+    private fun setupAlphabetIndex(latinNames: List<String>) {
+        val indexList = generateAlphabetIndex(latinNames)
 
-        val navExplore = findViewById<Button>(R.id.explore)
-        navExplore.setOnClickListener {
-            val intent = Intent(this, ExplorePlant::class.java)
-            startActivity(intent)
+        indexList.forEach { letter ->
+            val letterView = TextView(this)
+            letterView.text = letter.toString()
+            letterView.setOnClickListener {
+                val position = latinNames.indexOfFirst { it.startsWith(letter.toString(), ignoreCase = true) }
+                if (position != -1) {
+                    plantsListView.smoothScrollToPosition(position)
+                }
+            }
+            alphabetIndexLayout.addView(letterView)
         }
+    }
+
+    private fun generateAlphabetIndex(latinNames: List<String>): Set<Char> {
+        return latinNames.mapNotNull { it.firstOrNull()?.toUpperCase() }.toSortedSet()
     }
 }
